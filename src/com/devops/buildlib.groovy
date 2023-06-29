7caf9acf8064ae83f656e98862525211698795d8
Valentina_leal <valentinaleal@periferia-it.com>
2023-06-29 16:19:43 -0500

    cambio

--- src/com/devops/buildlib.groovy
+++ src/com/devops/buildlib.groovy
@@ -1,5 +1,5 @@
 package com.devops
 
-def hello(){
+def call(){
         sh 'npm install'
 }
--- vars/librerias.groovy
+++ vars/librerias.groovy
@@ -8,7 +8,7 @@ def call(Map params){
                 steps {
                     script {
                         def build = new com.devops.buildlib()
-                        build.hello(saludo:'hola')
+                        build()
                     }
                 }
                 
