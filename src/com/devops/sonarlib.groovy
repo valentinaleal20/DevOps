235929a9439f9aa7b9d531afee2328049623e1bc
Valentina_leal <valentinaleal@periferia-it.com>
2023-07-04 15:02:46 -0500

    cambio

--- src/com/devops/sonarlib.groovy
+++ src/com/devops/sonarlib.groovy
@@ -1,10 +1,10 @@
 package com.devops
 
 def call(script){
-    script.withSonarQubeEnv('sonarqube'){
+    script.withSonarQubeEnv('serversonarqube'){
         bat "mvn clean verify sonar:sonar"
         }
-    script.withSonarQubeEnv('sonarqube'){
+    script.withSonarQubeEnv('serversonarqube'){
         timeout(time: 3, unit: 'MINUTES'){
         waitForQualityGate abortPipeline: true 
         }
