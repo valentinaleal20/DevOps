ed1730094011ab0f5034a354a8d34d5b1b22f7d5
Valentina_leal <valentinaleal@periferia-it.com>
2023-07-04 17:23:18 -0500

    cambio

--- src/com/devops/imagen.groovy
+++ src/com/devops/imagen.groovy
@@ -1,11 +1,14 @@
 package com.devops
 
 def call() {
-    withCredentials([string(credentialsId: 'tockendockerhub', variable: 'DOCKER_TOKEN')]) {
+    withCredentials([string(credentialsId: 'tokendockerhub', variable: 'DOCKER_TOKEN')]) {
         sh "docker build -t fase2 ."
-        sh "docker login -u token -p $DOCKER_TOKEN"
-        sh 'docker run mi-docker-image npm test'
-        sh 'docker tag mi-docker-image username/mi-docker-image:latest'
-        sh 'docker push username/mi-docker-image:latest'
+        
+        docker.withRegistry('https://registry-1.docker.io', 'dockerhub-credentials') {
+            sh 'docker login -u token -p $DOCKER_TOKEN'
+            sh 'docker run mi-docker-image npm test'
+            sh 'docker tag mi-docker-image username/mi-docker-image:latest'
+            sh 'docker push username/mi-docker-image:latest'
+        }
     }
 }
