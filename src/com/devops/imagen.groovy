81486e275ccbe562b7e03c07d0c08e43cbb09f2e
Valentina_leal <valentinaleal@periferia-it.com>
2023-07-05 15:38:45 -0500

    cambio

--- src/com/devops/imagen.groovy
+++ src/com/devops/imagen.groovy
@@ -4,7 +4,7 @@ def call() {
     withCredentials([string(credentialsId: 'tockendockerhub', variable: 'DOCKER_TOKEN')]) {
         sh "docker build -t fase2 ."
         
-        docker.withRegistry('https://registry-1.docker.io', 'dockerhub-credentials') {
+        docker.withRegistry('https://registry-1.docker.io', 'dockerhub-tockendockerhub') {
             sh 'docker login -u token -p $DOCKER_TOKEN'
             sh 'docker run mi-docker-image npm test'
             sh 'docker tag mi-docker-image username/mi-docker-image:latest'
